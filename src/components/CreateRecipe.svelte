a9d2895cc32bfebf4396e786e979e83d2856829d
Alex Wormbs <alexander.wormbs@gmail.com>
2020-06-20 22:44:42 +0100

    temp

--- src/components/CreateRecipe.svelte
+++ src/components/CreateRecipe.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-
+  let title = "title";
+  let namez = "world";
 </script>
 
 <style lang="scss">
@@ -7,11 +8,45 @@
 
   .container {
     display: grid;
-    background-color: $main_dark3;
+    grid: auto / 1fr 1fr;
+    background-color: $accent_dark2;
     height: 100vh;
+    padding: 30px;
+    border-radius: $border-radius;
+  }
+
+  .recipe-title {
+    font-size: 35px;
+  }
+
+  .ingredients {
+    grid-column-start: 1;
+  }
+  .ingredients-title {
+    font-size: 20px;
+  }
+
+  .ingredients-box {
+    background-color: $grey;
+    border-radius: $border-radius;
+  }
+
+  input {
+    background-color: $accent_dark2;
+    font-size: 35px;
   }
 </style>
 
+<input bind:value={namez} />
+<h1>Hello {namez}!</h1>
+
 <div class="container">
-  <span></span>
+  <div class="recipe-title">
+    <input bind:value={title} placeholder="Recipe title" />
+  </div>
+  {title}
+  <div class="ingredients">
+    <div class="ingredients-title">Ingredients</div>
+    <div class="ingredients-box">awdw</div>
+  </div>
 </div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,14 +1,18 @@
 <script>
   import Header from "@components/Header.svelte";
+  let test = "test";
 </script>
 
 <style lang="scss" global>
   @import "@style/global.scss";
-  
+
   .header {
     margin-bottom: 50px;
   }
 </style>
+
+<textarea bind:value={test}></textarea>
+{test}
 
 <div class="header">
   <Header />
